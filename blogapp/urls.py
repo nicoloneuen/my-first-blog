d6870be2e9ee23f0bca1271deeb4391f1c528b28
nicoloneuen <nicoloneuen@gmail.com>
2019-05-07 22:43:16 +0100

    agrege template extensions, tambien la view de post_detail y el css.

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,4 +4,5 @@ from . import views #importa todas las views del directorio en el que estoy, ose
 urlpatterns = [
 #asignamos una view (post_list) al url root, y luego le asignamos un nombre leible
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #es el link que quiero en el html
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 #creamos una funcion post:list() que toma el request y devuelve return los valores llamando a la funcion render que pone todo junto en nuestro template el.html
@@ -10,6 +11,14 @@ def post_list(request):
 
 """
 # esta es la funcion sin el filtro solo ordenada por fecha de publicacion el - antes del argumento en order_by es descendente
+
+#vista para la lista de posts (el repeater)
 def post_list(request):
     posts = Post.objects.order_by('-published_date')
     return render(request, 'blogapp/post_list.html', {'posts': posts})
+
+#vista para el detalle del post
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    Post.objects.get(pk=pk)
+    return render(request, 'blogapp/post_detail.html', {'post': post})
