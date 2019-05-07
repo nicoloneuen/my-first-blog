988432ea7f2f9faa07d3c52b35273c83568cc62c
nicoloneuen <nicoloneuen@gmail.com>
2019-05-07 19:35:49 +0100

    se actualizo HTML template en blogapp con un repeater.

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 #creamos una funcion post:list() que toma el request y devuelve return los valores llamando a la funcion render que pone todo junto en nuestro template el.html
+""" ESTA ES LA FUNCION ORIGINAL
 def post_list(request):
-	return render(request, 'blogapp/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blogapp/post_list.html', {'posts': posts})
+
+"""
+# esta es la funcion sin el filtro solo ordenada por fecha de publicacion el - antes del argumento en order_by es descendente
+def post_list(request):
+    posts = Post.objects.order_by('-published_date')
+    return render(request, 'blogapp/post_list.html', {'posts': posts})
