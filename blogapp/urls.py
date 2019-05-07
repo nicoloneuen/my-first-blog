a616cf213f450ca01cbf3f7cbb5e7ec17ffa473f
nicoloneuen <nicoloneuen@gmail.com>
2019-05-07 08:14:32 +0100

    se cambio el HTML y se hicieron urls.

---
+++ blogapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path #importa la funcion path() de django.urls
+from . import views #importa todas las views del directorio en el que estoy, osea blogapp
+#url patterns
+urlpatterns = [
+#asignamos una view (post_list) al url root, y luego le asignamos un nombre leible
+    path('', views.post_list, name='post_list'),
+]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#creamos una funcion post:list() que toma el request y devuelve return los valores llamando a la funcion render que pone todo junto en nuestro template el.html
+def post_list(request):
+	return render(request, 'blogapp/post_list.html', {})
